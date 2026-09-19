8f58ffd11337dee020342fa1d72156bcfa7d95ad
lcmsarq-creator <lcms.arq@gmail.com>
2026-09-19 15:16:11 -0300

    fix: expor ponte de estado para organizador

--- fetch-base.js
+++ fetch-base.js
@@ -1,6 +1,16 @@
 (() => {
   "use strict";
   if (!window.__mvNativeFetch) window.__mvNativeFetch = window.fetch.bind(window);
+
+  const bridgeTimer = window.setInterval(() => {
+    try {
+      if (typeof state === "undefined" || typeof els === "undefined") return;
+      if (!window.state) window.state = state;
+      if (!window.els) window.els = els;
+      window.clearInterval(bridgeTimer);
+    } catch (_) {}
+  }, 50);
+
   if (!document.querySelector('script[data-mv-achievement-organization]')) {
     const script = document.createElement("script");
     script.dataset.mvAchievementOrganization = "true";

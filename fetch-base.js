037bcb1b5a4773c8237c1f30046f5c97aa035e5c
lcmsarq-creator <lcms.arq@gmail.com>
2026-09-19 15:15:45 -0300

    fix: carregar organizador de conquistas

--- fetch-base.js
+++ fetch-base.js
@@ -1,4 +1,11 @@
 (() => {
   "use strict";
   if (!window.__mvNativeFetch) window.__mvNativeFetch = window.fetch.bind(window);
+  if (!document.querySelector('script[data-mv-achievement-organization]')) {
+    const script = document.createElement("script");
+    script.dataset.mvAchievementOrganization = "true";
+    script.async = true;
+    script.src = `achievement-organization-hotfix.js?v=${encodeURIComponent(window.MINHAS_VIAGENS_APP_VERSION || "0.14.8")}`;
+    document.head.appendChild(script);
+  }
 })();
